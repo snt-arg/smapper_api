203a634fc0206f5826b83ff765dcab9258801504
Pedro Soares <pmbs.123@gmail.com>
2025-03-21 11:57:59 +0100

    feat: create services from configuration and add newly created routes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 from contextlib import asynccontextmanager
+import logging
 from typing import Annotated
 
 from fastapi import Depends, FastAPI
@@ -8,7 +9,11 @@ from fastapi.responses import HTMLResponse
 
 from app.routers import sensors
 from app.config.settings import APISettings
-from app.routers.v1 import sensors_router, bags_router, power_router
+from app.routers.v1 import sensors_router, bags_router, power_router, services_router
+from app.core.service_manager import get_service_manager
+from app.config.config import Configuration, RosService, Service
+
+logger = logging.getLogger("unvicorn.error")
 
 # To fix issue with CORS when requesting on the browser
 origins = [
@@ -20,9 +25,23 @@ origins = [
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("Before Start FASTAPI")
+    # read configuration
+    # setup services
+    # start services if autostart is true
+    manager = get_service_manager()
+
+    logger.info("Loading configuration")
+    config = Configuration.load("app/config/device.yaml")
+
+    for service in config.services:
+        if isinstance(service, Service):
+            manager.add_service(service.id, service.name, cmd=service.cmd)
+        elif isinstance(service, RosService):
+            manager.add_service(service.id, service.name, cmd=service.exec)
+
+    logger.info("Before Start FASTAPI")
     yield
-    print("After End FASTAPI")
+    logger.info("After End FASTAPI")
 
 
 @lru_cache
@@ -31,12 +50,13 @@ def get_settings():
 
 
 app = FastAPI(
-    debug=True,
+    debug=False,
     lifespan=lifespan,
 )
 
 
 app.include_router(sensors_router)
+app.include_router(services_router)
 app.include_router(bags_router)
 app.include_router(power_router)
 
