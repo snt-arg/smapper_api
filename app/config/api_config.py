24986a121b46e03ec8b06d0e5887ae1acf6bce66
Pedro Soares <pmbs.123@gmail.com>
2025-03-21 17:53:47 +0100

    feat: create a config for the API metadata

---
+++ app/config/api_config.py
@@ -0,0 +1,17 @@
+TITLE = "SMapper API"
+
+SUMMARY = "API for the SMapper handheld device"
+
+VERSION = "0.1.0"
+
+DESCRIPTION = """
+# SMapper API
+
+SMapper API is the main contact point with the hanheld device. It can be used 
+to check which sensors are currently installed, start/stop/restart a set of services
+and more, like powering off the onboard computer. For more information, you can visit
+the /docs endpoint for the device documentation
+"""
+
+API_DOCS_URL = "/api/docs"
+OPENAPI_URL = "/api/openapi.json"
