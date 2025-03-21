5751ac1f1541872b0b454c5872e83d936cf84032
Pedro Soares <pmbs.123@gmail.com>
2025-03-21 17:55:36 +0100

    refactor: use logger

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,17 +1,18 @@
 from functools import lru_cache
 from app.config.config import Configuration, load_config
 from app.core.service_manager import ServiceManager
+from app.logger import logger
 
 
 @lru_cache()
 def get_configuration() -> Configuration:
-    print("GET CONFIGURATION CALLED")
+    logger.debug("Get configuration dependency called")
     config = load_config("config/device_config.yaml")
     return config
 
 
 @lru_cache()
 def get_service_manager() -> ServiceManager:
-    print("GET SERVICE MANGER CALLED")
+    logger.debug("Get service manager dependency called")
     manager = ServiceManager()
     return manager
