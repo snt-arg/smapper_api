116a92d613e2f8e979bfe308faf09704373626ff
Pedro Soares <pmbs.123@gmail.com>
2025-03-21 11:57:00 +0100

    refactor: change sensors routes for newly created configuration

--- app/routers/v1/__init__.py
+++ app/routers/v1/__init__.py
@@ -1,5 +1,6 @@
 from .sensors import router as sensors_router
 from .bags import router as bags_router
 from .power import router as power_router
+from .services import router as services_router
 
-__all__ = ["sensors_router", "bags_router", "power_router"]
+__all__ = ["sensors_router", "bags_router", "power_router", "services_router"]
--- app/routers/v1/sensors.py
+++ app/routers/v1/sensors.py
@@ -1,34 +1,28 @@
-from fastapi import APIRouter
+from functools import lru_cache
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
+from app.config.config import Configuration
 
 
 router = APIRouter(prefix="/api/v1")
 
 
+@lru_cache()
+def get_configuration():
+    return Configuration.load("app/config/device.yaml")
+
+
 @router.get("/sensors", description="Get a list of available sensors")
-def get_sensors():
-    return ["lidar", "argus_cameras", "realsense"]
+def get_sensors(config: Annotated[Configuration, Depends(get_configuration)]):
+    return config.sensors
 
 
 @router.get("/sensors/{sensor_name}", description="Get metadata of given sensor")
-def get_sensor(sensor_name: str):
-    # Get actual sensor metadata
-    metadata = {
-        "name": "lidar",
-        "type": "3d lidar",
-        "model": "Ouster OS0",
-        "service": "ACTIVE",
-    }
+def get_sensor(
+    sensor_name: str, config: Annotated[Configuration, Depends(get_configuration)]
+):
+    for sensor in config.sensors:
+        if sensor.name == sensor_name:
+            return sensor
 
-    return metadata
-
-
-@router.post("/sensors/{sensor_name}/start", description="Start sensor service")
-def start_sensor(sensor_name: str):
-    # Start sensor service with ProcessPool
-    return {"status": "OK"}
-
-
-@router.post("/sensors/{sensor_name}/stop", description="Stop sensor service")
-def stop_sensor(sensor_name: str):
-    # Stop sensor service with ProcessPool
-    return {"status": "OK"}
+    raise HTTPException(404, detail=f"Sensor with name <{sensor_name}> does not exist")
