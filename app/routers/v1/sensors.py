4512da4a93237dd747613ad9a121415c90c98c44
Pedro Soares <pmbs.123@gmail.com>
2025-03-23 16:56:25 +0100

    feat: annotate endpoints

--- app/routers/v1/sensors.py
+++ app/routers/v1/sensors.py
@@ -1,22 +1,25 @@
 from functools import lru_cache
-from typing import Annotated
+from typing import Annotated, List
 from fastapi import APIRouter, Depends, HTTPException
 from app.config.settings import DeviceSettings
 from app.dependencies import get_device_settings
+from app.schemas.sensors import SensorSchema
 
 
 router = APIRouter(prefix="/api/v1")
 
 
 @router.get("/sensors", description="Get a list of available sensors")
-def get_sensors(config: Annotated[DeviceSettings, Depends(get_device_settings)]):
+def get_sensors(
+    config: Annotated[DeviceSettings, Depends(get_device_settings)]
+) -> List[SensorSchema]:
     return config.sensors
 
 
 @router.get("/sensors/{sensor_name}", description="Get metadata of given sensor")
 def get_sensor(
     sensor_name: str, config: Annotated[DeviceSettings, Depends(get_device_settings)]
-):
+) -> SensorSchema:
     for sensor in config.sensors:
         if sensor.name == sensor_name:
             return sensor
--- app/routers/v1/services.py
+++ app/routers/v1/services.py
@@ -1,5 +1,5 @@
 from functools import lru_cache
-from typing import Annotated
+from typing import Annotated, List
 from fastapi import APIRouter, Depends, HTTPException
 from app.config.settings import DeviceSettings
 from app.core.service_manager import (
@@ -8,18 +8,29 @@ from app.core.service_manager import (
 )
 from app.core.services.service import ServiceState
 from app.dependencies import get_device_settings, get_service_manager
+from app.schemas.services import RosServiceSchema, ServiceSchema
 
 
 router = APIRouter(prefix="/api/v1")
 
 
 @router.get("/services", description="Get a list of available services")
-def get_sensors(config: Annotated[DeviceSettings, Depends(get_device_settings)]):
+def get_services(
+    config: Annotated[DeviceSettings, Depends(get_device_settings)]
+) -> List[ServiceSchema | RosServiceSchema]:
     return config.services
 
 
 @router.get("/services/{id}", description="Get current state of service with id")
-def get_sensor(
+def get_service_by_id(
+    id: str,
+    manager: Annotated[ServiceManager, Depends(get_service_manager)],
+) -> ServiceSchema:
+    raise HTTPException(404, detail=f"Service {id} not found.")
+
+
+@router.get("/services/{id}/state", description="Get current state of service with id")
+def get_service_state_by_id(
     id: str,
     manager: Annotated[ServiceManager, Depends(get_service_manager)],
 ) -> ServiceState:
@@ -31,16 +42,20 @@ def get_sensor(
 
 
 @router.post("/services/{id}/start", description="Start service with id")
-def start_sensor(
+def start_service_with_id(
     id: str,
     manager: Annotated[ServiceManager, Depends(get_service_manager)],
-):
-    return manager.start_service(id)
+) -> ServiceSchema:
+    manager.start_service(id)
+    # TODO: return service state once it is implemented in service_manager
+    return
 
 
 @router.post("/services/{id}/stop", description="Stop service with id")
-def stop_sensor(
+def stop_service_with_id(
     id: str,
     manager: Annotated[ServiceManager, Depends(get_service_manager)],
-):
-    return manager.stop_service(id)
+) -> ServiceSchema:
+    manager.stop_service(id)
+    # TODO: return service state once it is implemented in service_manager
+    return
