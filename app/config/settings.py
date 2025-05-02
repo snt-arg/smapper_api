f59bceca7229032bcbde9a21da130aea4864f98c
Pedro Soares <pmbs.123@gmail.com>
2025-05-02 09:45:29 +0200

    feat: add support to load different configs
    specify an env variable API_SETTINGS_NAME with the name being the
    settings file name under config. e.g.: settings.yaml

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,3 +1,5 @@
+import os
+import dotenv
 from typing import Tuple, Type
 
 from pydantic_settings import (
@@ -10,6 +12,21 @@ from pydantic_settings import (
 from app.config.managers_settings import ManagersSettings
 from app.config.device_settings import DeviceSettings
 from app.config.api_settings import APISettings
+from app.logging import logger
+
+
+def get_settings_file_name():
+    dotenv.load_dotenv()
+    name = os.getenv("API_SETTINGS_NAME")
+    if name:
+        if name in os.listdir("./config"):
+            logger.info(f"Loading custom API settings from config/{name}")
+            return f"config/{name}"
+    logger.info(f"Loading default API settings from config/settings.yaml")
+    return "config/settings.yaml"
+
+
+SETTINGS_FILE = os.getenv("API_SETTINGS_NAME")
 
 
 class AppSettings(BaseSettings):
@@ -17,7 +34,7 @@ class AppSettings(BaseSettings):
     managers: ManagersSettings = ManagersSettings()
     device: DeviceSettings = DeviceSettings()
 
-    model_config = SettingsConfigDict(yaml_file="config/settings.yaml")
+    model_config = SettingsConfigDict(yaml_file=get_settings_file_name())
 
     @classmethod
     def settings_customise_sources(
